4f5e8a4fe92a98e0b3aff23fff1dfe9204715a17
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-08-29 02:04:28 -0500

    empty line at eof

--- chapter01/getenv_demo.c
+++ chapter01/getenv_demo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
     char *shell = getenv("SHELL");
     printf("The current shell is %s.\n", shell);
-    
+
     return EXIT_SUCCESS;
 }
