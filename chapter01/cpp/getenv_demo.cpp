ee0897820cf45ebd3e12c854f8e2236cb96d2ffb
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-08-28 23:36:01 -0500

    updated cpp, c20

--- chapter01/cpp/getenv_demo.cpp
+++ chapter01/cpp/getenv_demo.cpp
@@ -3,11 +3,11 @@
 #include <iomanip>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
     char *shell = getenv("SHELL");
     // C++11/14/17
-    std::cout << "The current shell is "<< shell << std::endl;
-    
+    std::cout << "The current shell is " << shell << "." << std::endl;
+
     return EXIT_SUCCESS;
 }
